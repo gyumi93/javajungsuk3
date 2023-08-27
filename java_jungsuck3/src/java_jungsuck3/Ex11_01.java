5c0c162799b022c6a27b074247ffff6ad28ad8b6
gyumi93 <gyumikim93@naver.com>
2023-08-27 22:49:57 +0900

    2023-08-27

--- java_jungsuck3/src/java_jungsuck3/Ex11_01.java
+++ java_jungsuck3/src/java_jungsuck3/Ex11_01.java
@@ -42,9 +42,9 @@ class ArrayListEx1 {
 		}
 		print (list1, list2);
 	}
-	static void print (ArrayList list1, ArrayList list2) {
-		System.out.println("list1:"+list1);
-		System.out.println("list2:"+list2);
-		System.out.println();
+//	static void print (ArrayList list1, ArrayList list2) {
+//		System.out.println("list1:"+list1);
+//		System.out.println("list2:"+list2);
+//		System.out.println();
 		}
 	}
