ba58932bce222d442e531e24ade623276c544f16
gyumi93 <gyumikim93@naver.com>
2023-09-03 12:49:57 +0900

    2023-09-03

--- java_jungsuck3/src/java_jungsuck3/Ex12_7.java
+++ java_jungsuck3/src/java_jungsuck3/Ex12_7.java
@@ -8,16 +8,16 @@ class Child extends Parent {
 	void parentmethod() {} // 조상 메서드의 이름을 잘못 적었음.
 }
 
-@FunctionalInterface // 함수형 인터페이스는 하나의 추상 메서드만 가능
-interface Testable {
-	void test(); // 추상메서드
-	void check(); // 추상메서드
-}
-
-class Ex12_7 {
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
-		Child c = new Child();
-		c.parentmethod(); // deprecated된 메서드 사용
-	}
+//@FunctionalInterface // 함수형 인터페이스는 하나의 추상 메서드만 가능
+//interface Testable {
+//	void test(); // 추상메서드
+//	void check(); // 추상메서드
+//}
+//
+//class Ex12_7 {
+//	@SuppressWarnings("deprecation")
+//	public static void main(String[] args) {
+//		Child c = new Child();
+//		c.parentmethod(); // deprecated된 메서드 사용
+//	}
 }
