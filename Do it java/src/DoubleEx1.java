d95955cd9c3c0d53d696bcf187a10d28d40626fe
gyumi93 <gyumikim93@naver.com>
2023-09-20 23:41:26 +0900

    2023-09-20

---
+++ Do it java/src/DoubleEx1.java
@@ -0,0 +1,12 @@
+
+public class DoubleEx1 {
+
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14F; // F 또는 f를 숫자 뒤에 붙여서 식별해주어야 한다. 접미사 또는 식별자라고 부른다.
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+
+}
