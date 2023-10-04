6c1ba8141376f48cc946615be6c5cf6c7ba01c19
gyumi93 <gyumikim93@naver.com>
2023-10-05 00:05:02 +0900

    2023-10-04

--- Do it java/src/Chapter2_Q5.java
+++ Do it java/src/Chapter2_Q5.java
@@ -2,7 +2,7 @@
 public class Chapter2_Q5 {
 
 	public static void main(String[] args) {
-		char ch = '\uAE00';
-//		System.out.println("A");
+//		char ch = '\uAE00';
+		System.out.println("A");
 	}
 }
