31a9b3ac7addfc3967c6bbabfb6df5041f387ce8
gyumi93 <gyumikim93@naver.com>
2023-10-06 23:04:04 +0900

    2023-10-06

--- Do it java/src/Chapter2_Q5.java
+++ Do it java/src/Chapter2_Q5.java
@@ -2,10 +2,10 @@
 public class Chapter2_Q5 {
 
 	public static void main(String[] args) {
-//		char ch = '\uAE00';
-//		System.out.println();
-//		System.out.println();
-//		char ch = 'a';
-////		System.out.println("A");
+		char ch = '\uAE00';
+		System.out.println();
+		System.out.println();
+		char ch = 'a';
+//		System.out.println("A");
 	}
 }
